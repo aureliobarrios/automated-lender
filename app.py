064515f436de262bafafcb41aa3dec6305d74968
aureliobarrios <aubarrios142@gmail.com>
2025-10-09 17:07:01 -0700

    app

---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+    # ----- Helper Functions -----
+
+    # ----- Components -----
+
+    # ----- Functions -----
+
+    # ----- Actions -----
+    pass
+
+if __name__ == "__main__":
+    demo.launch(show_error=True)
